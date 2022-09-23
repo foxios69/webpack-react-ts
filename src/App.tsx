7f0d6f573429de05cc3f7730c3ee5b8e5ec6b9dc
Nikita <nikita.gavrilov@nordclan.com>
2022-09-23 15:11:25 +0300

    update: added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import "./index.scss";
-import "./index.css";
-import avatar from "./assets/image.png"
-import parrot from "./assets/detailed-parrot-polyprismatic.svg"
-
+import './index.scss';
+import './index.css';
+import avatar from './assets/image.png';
+import parrot from './assets/detailed-parrot-polyprismatic.svg';
 
 export const App = () => {
-
-    return (
-        <>  <h1>My React and TypeScript App!</h1>
-            <p>kek</p>
-            <img src={avatar}></img>
-            <img src={parrot}></img>
-
-        </>)
-}
+  const arr = [1, 1, 1, 1, 11, 1, 1, , 11, 1];
+  return (
+    <>
+      {' '}
+      <h1>My React and TypeScript App!</h1>
+      <p>kek</p>
+      <img src={avatar}></img>
+      <img src={parrot}></img>
+    </>
+  );
+};
